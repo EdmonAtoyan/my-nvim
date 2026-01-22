c73edb6a0694e7dc800e7a0f55876c643d2c1067
EdmonAtoyan <edmon.atoyan.00@gmail.com>
2026-01-22 22:57:49 +0400

    V1

---
+++ nvim/init.lua
@@ -0,0 +1,25 @@
+-- Basic settings
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.termguicolors = true
+vim.opt.signcolumn = "yes"
+vim.opt.cursorline = true
+
+vim.g.mapleader = " "
+
+-- Lazy.nvim bootstrap
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "https://github.com/folke/lazy.nvim.git",
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup("plugins")
+
